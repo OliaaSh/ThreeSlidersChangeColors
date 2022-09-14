b63861d53ddb36ef1fde5a9942ad99fd21c1f5c7
Oliaa <olga.paskova@gmail.com>
2022-09-14 21:22:57 +0300

    final

--- ThreeSlidersChangeColors/ViewController.swift
+++ ThreeSlidersChangeColors/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorScreen: UIView!
+    
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var greenColorValue: UILabel!
+    @IBOutlet var blueColorValue: UILabel!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorScreen.layer.cornerRadius = 10
+        
+        redColorValue.text = String(redColorSlider.value)
+        greenColorValue.text = String(greenColorSlider.value)
+        blueColorValue.text = String(blueColorSlider.value)
+        
+        redColorSlider.minimumTrackTintColor = .red
+        greenColorSlider.minimumTrackTintColor = .green
+        blueColorSlider.minimumTrackTintColor = .blue
+      }
+    
+    
+    @IBAction func redColorChange() {
+        redColorValue.text = String(round(redColorSlider.value*1000)/1000)
+        colorScreen.backgroundColor = .red
+        
+        let redColor = CGFloat(redColorSlider.value)
+        let greenColor = CGFloat(greenColorSlider.value)
+        let blueColor = CGFloat(blueColorSlider.value)
+        
+       colorScreen.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1.0)
     }
-
-
+    
+    
+    @IBAction func greenColorChange() {
+        greenColorValue.text = String(round(greenColorSlider.value*1000)/1000)
+        colorScreen.backgroundColor = .green
+        
+        let redColor = CGFloat(redColorSlider.value)
+        let greenColor = CGFloat(greenColorSlider.value)
+        let blueColor = CGFloat(blueColorSlider.value)
+        
+        colorScreen.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1.0)
+    }
+    
+    
+    @IBAction func blueColorChange() {
+        blueColorValue.text = String(round(blueColorSlider.value*1000)/1000)
+        colorScreen.backgroundColor = .blue
+        
+        let redColor = CGFloat(redColorSlider.value)
+        let greenColor = CGFloat(greenColorSlider.value)
+        let blueColor = CGFloat(blueColorSlider.value)
+        
+        colorScreen.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1.0)
+    }
+    
 }
 
